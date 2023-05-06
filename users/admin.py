80db2ca909ee68f9a2d69aec011e147e7aecdd10
DONOTFORGET-BYTE <70011086+DONOTFORGET-BYTE@users.noreply.github.com>
2023-05-06 22:27:25 +0200

    feat: add user activity model to admin

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Profile
+from .models import UserActivity
+
+# Register models to the admin panel
 
 admin.site.register(Profile)
+admin.site.register(UserActivity)
