ca2ba4df57395156597a5532cf3723d53454113e
DONOTFORGET-BYTE <70011086+DONOTFORGET-BYTE@users.noreply.github.com>
2023-05-07 15:28:06 +0200

    feat: add modal path

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, profile, RegisterView
+from .views import home, profile, RegisterView, evoke_modal
 
 urlpatterns = [
     path('', home, name='users-home'),
     path('register/', RegisterView.as_view(), name='users-register'),
     path('profile/', profile, name='users-profile'),
+    path('evoke/', evoke_modal, name='evoke_modal')
 ]
